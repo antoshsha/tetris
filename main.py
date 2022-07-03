3d3e7f1efd103d234cdda52461c35674e6a35147
AntonGog171 <antongogol2017@gmail.com>
2022-07-03 14:21:18 +0300

    added not working menu

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pygame
 from copy import deepcopy
 import random
+import time
+import ctypes
+
+ctypes.windll.user32.SetProcessDPIAware()
 WIDTH, HEIGHT = 10, 20
 TILE = 30
-GAME_RES = WIDTH*TILE, HEIGHT*TILE
+GAME_RES = WIDTH * TILE, HEIGHT * TILE
 FPS = 60
-RES = 1100, 756
+RES = 1100, 753
 pygame.init()
 main_font = pygame.font.Font('fonts/Cheltenham-Normal.ttf', 65)
+small_font= pygame.font.Font('fonts/Cheltenham-Normal.ttf', 30)
 title = main_font.render('CATETRIS', True, pygame.Color('black'))
 titleScore = main_font.render('score', True, pygame.Color('red'))
 titleRecord = main_font.render('record', True, pygame.Color('green'))
-get_color = lambda : (random.randrange(30, 256), random.randrange(30, 256), random.randrange(30, 256))
+get_color = lambda: (random.randrange(30, 256), random.randrange(30, 256), random.randrange(30, 256))
 color = get_color()
 count, speed, limit = 0, 60, 2000
 p = [[0 for i in range(WIDTH)] for i1 in range(HEIGHT)]
+pygame.display.set_caption('Title of window')
 screen = pygame.display.set_mode(RES)
 gameScreen = pygame.Surface(GAME_RES)
+menuscreen = pygame.Surface(GAME_RES)
 clock = pygame.time.Clock()
-grid = [pygame.Rect(x*TILE, y*TILE, TILE, TILE) for x in range(WIDTH) for y in range(HEIGHT)]
+grid = [pygame.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(WIDTH) for y in range(HEIGHT)]
 figuresCoord = [
     [(0, 0), (0, -1), (0, 1), (-1, 0)],
     [(0, 0), (0, -1), (0, 1), (1, -1)],
@@ -28,17 +35,19 @@ figuresCoord = [
     [(0, -1), (-1, -1), (-1, 0), (0, 0)],
     [(-1, 0), (-2, 0), (0, 0), (1, 0)],
 ]
+
 bg1 = pygame.image.load('images/1.jpg').convert()
 bg2 = pygame.image.load('images/11.jpg').convert()
+bg3 = pygame.image.load('images/2.jpg').convert()
+
+
 def borders():
-    if figure[i].x < 0 or figure[i].x > WIDTH-1:
+    if figure[i].x < 0 or figure[i].x > WIDTH - 1:
         return True
     elif figure[i].y > HEIGHT - 1 or p[figure[i].y][figure[i].x]:
 
         return True
     return False
-
-
 
 
 def record():
@@ -56,13 +65,10 @@ def set_record(rec, scor):
         f.write(str(rek))
 
 
-    
-
-
 figures = [[pygame.Rect(x + WIDTH // 2, y + 1, 1, 1) for x, y in i] for i in figuresCoord]
 rectfig = pygame.Rect(0, 0, TILE - 2, TILE - 2)
 figure = deepcopy(random.choice(figures))
-#score
+# score
 score, line = 0, 0
 scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
 while True:
@@ -71,7 +77,8 @@ while True:
     screen.blit(gameScreen, (45, 156))
     gameScreen.blit(bg2, (0, 0))
 
-#delay
+
+    # delay
     for i in range(line):
         pygame.time.wait(200)
     xx, r = 0, False
@@ -136,7 +143,7 @@ while True:
     # //////
     # delete full lines
     l_quantity, line = HEIGHT - 1, 0
-    for row in range(HEIGHT-1, -1, -1):
+    for row in range(HEIGHT - 1, -1, -1):
         counter = 0
         for i in range(WIDTH):
             if p[row][i]:
@@ -147,24 +154,34 @@ while True:
         else:
             speed += 3
             line += 1
-    #scooore
+    # scooore
     score += scores[line]
     # delete full lines
 
-    #game over :(
-    for i in range(WIDTH):
-        if p[0][i]:
-            set_record(recordd, score)
-            p = [[0 for i in range(WIDTH)] for i1 in range(HEIGHT)]
-            count, speed, limit = 0, 60, 2000
-            score = 0
-    # game over :(
+
     screen.blit(title, (350, 20))
     screen.blit(titleScore, (420, 90))
     screen.blit(main_font.render(str(score), True, pygame.Color('black')), (420, 170))
     screen.blit(titleRecord, (700, 90))
     screen.blit(main_font.render(str(recordd), True, pygame.Color('black')), (950, 90))
     pygame.display.flip()
+    # game over :(
+    for i in range(WIDTH):
+        if p[0][i]:
+            set_record(recordd, score)
+            p = [[0 for i in range(WIDTH)] for i1 in range(HEIGHT)]
+            count, speed, limit = 0, 60, 2000
+            score = 0
+            screen.blit(menuscreen, (45, 156))
+            menuscreen.blit(bg3, (0, 0))
+            menuscreen.blit(small_font.render('Play again?', True, pygame.Color('black')), (60, 180))
+            pygame.display.flip()
+            time.sleep(3)
+
+
+
+
+
 
 clock.tick(FPS)
 
